e5bf1161f0748cf8526e9d9ca998014f6902a9d9
Samir Talwar <samir@noodlesandwich.com>
2022-07-06 23:04:02 +0200

    diy: Implement a server-side counter with a GraphQL API.

--- diy/index.ts
+++ diy/index.ts
@@ -3,6 +3,16 @@ import Koa from "koa";
 import Router from "@koa/router";
 import * as gql from "graphql";
 
+interface Counter {
+  count: number;
+}
+
+const initialState = (): Counter => ({
+  count: 0,
+});
+
+let state = initialState();
+
 const consumeStream = (input: stream.Readable): Promise<string> =>
   new Promise((resolve, reject) => {
     const buffer: string[] = [];
@@ -18,14 +28,46 @@ const consumeStream = (input: stream.Readable): Promise<string> =>
     input.on("error", reject);
   });
 
+const counterType = new gql.GraphQLObjectType({
+  name: "Counter",
+  fields: {
+    count: {
+      type: new gql.GraphQLNonNull(gql.GraphQLInt),
+    },
+  },
+});
+
 const schema = new gql.GraphQLSchema({
   query: new gql.GraphQLObjectType({
     name: "RootQueryType",
     fields: {
-      hello: {
-        type: gql.GraphQLString,
+      counter: {
+        type: counterType,
         resolve() {
-          return "world";
+          return state;
+        },
+      },
+    },
+  }),
+  mutation: new gql.GraphQLObjectType({
+    name: "RootMutationType",
+    fields: {
+      increment: {
+        type: counterType,
+        args: { by: { type: gql.GraphQLInt } },
+        resolve(_, increment) {
+          if (increment.by != null && increment.by <= 0) {
+            throw new Error("Cannot increment by zero or a negative number.");
+          }
+          state.count += increment.by || 1;
+          return state;
+        },
+      },
+      reset: {
+        type: counterType,
+        resolve() {
+          state = initialState();
+          return state;
         },
       },
     },
@@ -34,9 +76,7 @@ const schema = new gql.GraphQLSchema({
 
 const router = new Router()
   .get("/", async (ctx) => {
-    ctx.body = {
-      hello: "world",
-    };
+    ctx.body = "Nothing to see here.";
   })
   .post("/graphql", async (ctx) => {
     if (!ctx.request.is("application/graphql")) {
